e0d8f857699947410224f59cd8e86f7bb2a3704c
Alexander Tobek <sasha.torbek@yandex.ru>
2019-03-16 12:37:28 +0300

    fix carousel

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ymaps.ready(() => {
     let oPoints = {};
     let map = new ymaps.Map('map', {
         center: [55.76, 37.64],
-        zoom: 7
+        zoom: 5
     }, {});
 
     let customContentLayout = ymaps.templateLayoutFactory.createClass(
@@ -40,52 +40,6 @@ ymaps.ready(() => {
 
     map.geoObjects.add(oCluster);
 
-    let addCommentOnForm = () => {
-        let pId = point.properties.get('id');
-
-        if (oPoints.hasOwnProperty(pId)) {
-            oPoints[pId].comments.push({
-                name: name.value,
-                place: place.value,
-                comment: comment.value,
-                date: date
-            });
-
-            point = new ymaps.Placemark(point.geometry.getCoordinates(), {
-                id: ++incId,
-                parentId: pId,
-                location: point.properties.get('location'),
-                balloonContentHeader: `<a href="" id='ballonHeader'>${point.properties.get('location')}</a>`,
-                balloonContentBody: comment.value,
-                balloonContentFooter: date
-            }, {
-                preset: 'islands#violetIcon',
-            });
-
-            oCluster.add(point);
-
-        } else {
-            oPoints[pId] = {
-                location: point.properties.get('location'),
-                comments: [{
-                    name: name.value,
-                    place: place.value,
-                    comment: comment.value,
-                    date: date
-                }]
-            };
-
-            point.properties.set({
-                'balloonContentHeader': `<a href="" id='ballonHeader'>${point.properties.get('location')}</a>`,
-                'balloonContentBody': comment.value,
-                balloonContentFooter: date
-            });
-            oCluster.add(point);
-        }
-
-        feedback.innerHTML = feedbackTempl(oPoints[pId]);
-    };
-
     let displayCommentForm = (l = '0', t = '0', d = 'none', oPoint) => {
         feedback.innerHTML = oPoint;
         feedback.style.left = l;
@@ -101,6 +55,52 @@ ymaps.ready(() => {
             `${date.getFullYear()}.${m}.${d}`,
             `${date.getHours()}:${m}:${date.getSeconds()}`
         ].join(' ');
+    };
+
+    let addCommentOnForm = () => {
+        let pId = point.properties.get('id');
+
+        if (oPoints.hasOwnProperty(pId)) {
+            oPoints[pId].comments.push({
+                name: name.value,
+                place: place.value,
+                comment: comment.value,
+                date: date
+            });
+
+            let newPoint = new ymaps.Placemark(point.geometry.getCoordinates(), {
+                id: ++incId,
+                parentId: pId,
+                location: point.properties.get('location'),
+                balloonContentHeader: `<a href="${pId}" id='ballonHeader'  data-index-number='${pId}'>${point.properties.get('location')}</a>`,
+                balloonContentBody: comment.value,
+                balloonContentFooter: date
+            }, {
+                preset: 'islands#violetIcon',
+            });
+
+            oCluster.add(newPoint);
+
+        } else {
+            oPoints[pId] = {
+                location: point.properties.get('location'),
+                comments: [{
+                    name: name.value,
+                    place: place.value,
+                    comment: comment.value,
+                    date: date
+                }]
+            };
+
+            point.properties.set({
+                'balloonContentHeader': `<a href="" id='ballonHeader' data-index-number='${pId}'>${point.properties.get('location')}</a>`,
+                'balloonContentBody': comment.value,
+                balloonContentFooter: date
+            });
+            oCluster.add(point);
+        }
+
+        feedback.innerHTML = feedbackTempl(oPoints[pId]);
     };
     
     map.events.add('click', e => {
@@ -141,10 +141,26 @@ ymaps.ready(() => {
 
         if (e.target.id === 'add-comment') {
             addCommentOnForm();
+            
+            return;
         }
 
         if (e.target.id === 'close') {
             displayCommentForm();
+            
+            return;
+        }
+
+        if (e.target.id === 'ballonHeader') {
+
+            e.preventDefault();
+            let pagePxls = [e.clientX, e.clientY];
+
+            console.log(e);
+
+            let fbTempl = feedbackTempl(oPoints[e.target.dataset.indexNumber]);
+
+            displayCommentForm(`${pagePxls[0]}px`, `${pagePxls[1]}px`, 'block', fbTempl);
         }
     });
 });
